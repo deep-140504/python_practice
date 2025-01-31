a279f6ceec58a5082b9eda96b664354a17768235
Deep Dabhi <deep.d@crestskillserve.com>
2025-01-31 17:35:43 +0530

    completed till pandas filter

--- W3/temp.py
+++ W3/temp.py
@@ -1,10 +1,5 @@
 import numpy as np
 
-dtype = [('name', 'U10'), ('age', 'i4'), ('height', 'f4')]
-arr = np.array([('a', 21, 5.6), ('b', 22, 5.7), ('c', 23, 5.8)], dtype=dtype)
-
-arr = np.core.records.fromarrays(arr.transpose(), dtype=dtype + [('name_age', 'U13')])
-
-arr['name_age'] = arr['name'] + " " + arr['age'].astype(str)
-
-print(arr)
+# Convert the list into a row vector
+row_vector = np.array([0, 1, 2])[None, :]
+print(row_vector)
